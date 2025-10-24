96426c16d0df8b2614394a523b7c89ba83e9aa0e
kwonsoojung <ydyd1357@naver.com>
2025-10-24 10:51:35 +0900

    2

--- toyProject1_backend2/src/main/java/toy/project/apiserver/domain/common/menu/repository/MenuRepository.java
+++
@@ -1,9 +0,0 @@
-package toy.project.apiserver.domain.common.menu.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import toy.project.apiserver.domain.common.menu.entity.MenuAgentEntity;
-
-public interface MenuRepository extends JpaRepository<MenuAgentEntity, Integer> {
-
-}
